beedcd03f628c344118407ff5f02515a293347d2
Студенты <Students@teach.local>
2024-11-12 16:29:45 +0300

    Create MasterPolAccountManager

---
+++ MasterPolAccountManager/MasterPolAccountManager/CalculateMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterPolAccountManager
+{
+    public class CalculateMaterial
+    {
+        public int Calculate(int IdTypeOfProduct, int IdTypeOfMaterial, int CountProducts, double param1, double param2)
+        {
+            try
+            {
+                double countOn1 = param1 * param2 * Convert.ToDouble(Data.ProductsTrainingEntities.GetContext().ProductType.Where(d => d.ID == IdTypeOfProduct).FirstOrDefault());
+
+                countOn1 += (param1 * param2 * Convert.ToDouble(Data.ProductsTrainingEntities.GetContext().ProductType.Where(d => d.ID == IdTypeOfProduct).FirstOrDefault())) * 
+                    Convert.ToDouble(Data.ProductsTrainingEntities.GetContext().TypeMaterial.Where(d => d.ID == IdTypeOfMaterial).FirstOrDefault());
+
+                return Convert.ToInt32(Math.Round(countOn1 * CountProducts));
+            }
+            catch (Exception ex) 
+            {
+            return -1;
+            }
+        }
+    }
+}
